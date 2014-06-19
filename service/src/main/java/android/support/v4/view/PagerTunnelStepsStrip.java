f687cd880e71f448dca935430a27c1b221be4667
Cyril Vlaminck <vlaminck.cyril@gmail.com>
2014-06-19 17:56:26 +0200

    WIP : PagerTunnelStepsStrip
    Support substeps
    Support XML attributes

--- service/src/main/java/android/support/v4/view/PagerTunnelStepsStrip.java
+++ service/src/main/java/android/support/v4/view/PagerTunnelStepsStrip.java
@@ -1,14 +1,18 @@
 package android.support.v4.view;
 
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.RectF;
+import android.text.TextPaint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.ViewParent;
+
+import fr.cvlaminck.notidroid.android.base.R;
 
 /**
  * View pager indicator that shows the view pager as a tunnel with steps.
@@ -31,29 +35,32 @@ public class PagerTunnelStepsStrip
     public final static String SUBSTEP_TITLE = "-";
 
     /**
-     * Color of dots representing both steps and substeps in the tunnel.
+     * Color of a step/substep when the user has not reached it. This color
+     * will to the stepColor when the user is navigating to this step/substep.
+     */
+    private int stepBackgroundColor = 0;
+
+    /**
+     * Color of a step/substep when this is the current step or this step has been completed
+     * by the user.
      */
     private int stepColor = 0;
 
     /**
-     * Color of dots representing both steps and substeps in the tunnel when this
-     * is the current step or this step has been completed.
+     * Diameter of the circle representing a step in the tunnel.
+     * Value in px.
      */
-    private int enabledStepColor = 0;
+    private int stepDiameter = 150;
 
     /**
-     * Radius of a dot representing a step in the tunnel in px.
+     * Diameter of the circle representing a substep in the tunnel.
+     * Value in px.
      */
-    private int stepDotDiameter = 150;
+    private int subStepDiameter = 5;
 
     /**
-     * Radius of a dot representing a substep in px.
-     */
-    private int subStepDotDiameter = 5;
-
-    /**
-     * Color of the number of the step. This number is displayed in the dot
-     * representing the step.
+     * Color of the number of a step. Only step are numbered and this number
+     * is displayed inside the circle using this color.
      */
     private int textColor = 0;
 
@@ -89,15 +96,44 @@ public class PagerTunnelStepsStrip
      */
     private float lastPositionOffset = 0;
 
+    /**
+     * Paint that will be used to draw step shape background on the canvas
+     */
+    final Paint stepBackgroundPaint = new Paint();
+
+    /**
+     * Paint that will be used to draw step shape foreground on the canvas
+     */
+    final Paint stepPaint = new Paint();
+
+    /**
+     * Paint that will be used to draw the step number over the step shape.
+     */
+    final TextPaint textPaint = new TextPaint();
+
 
     public PagerTunnelStepsStrip(Context context, AttributeSet attrs) {
         super(context, attrs);
+        computeAttributeSet(attrs);
         pageListener = new PageListener();
     }
 
     public PagerTunnelStepsStrip(Context context) {
         super(context);
+        computeAttributeSet(null);
         pageListener = new PageListener();
+    }
+
+    private void computeAttributeSet(AttributeSet attrs) {
+        //We read the attributes using the styleable we have created for this view.
+        TypedArray attributes = getContext().obtainStyledAttributes(attrs, R.styleable.PagerTunnelStepsStrip);
+        setStepBackgroundColor(stepBackgroundColor = attributes.getColor(R.styleable.PagerTunnelStepsStrip_backgroundStepColor, Color.LTGRAY));
+        setStepColor(attributes.getColor(R.styleable.PagerTunnelStepsStrip_stepColor, Color.GREEN));
+        stepDiameter = attributes.getDimensionPixelSize(R.styleable.PagerTunnelStepsStrip_stepDiameter, 50); //TODO ; put the default value in dp
+        subStepDiameter = attributes.getDimensionPixelSize(R.styleable.PagerTunnelStepsStrip_subStepDiameter, stepDiameter / 2);
+        textSize = attributes.getDimensionPixelSize(R.styleable.PagerTunnelStepsStrip_android_textSize, 16); //TODO : put the default value in sp
+        setTextColor(attributes.getColor(R.styleable.PagerTunnelStepsStrip_android_textColor, Color.WHITE));
+        attributes.recycle();
     }
 
     @Override
@@ -115,7 +151,7 @@ public class PagerTunnelStepsStrip
             setMeasuredDimension(widthSize, heightSize);
         } else {
             //The height of our component is equal to the radius of step dot + top/bottom padding
-            final int height = stepDotDiameter + getPaddingTop() + getPaddingBottom();
+            final int height = stepDiameter + getPaddingTop() + getPaddingBottom();
             setMeasuredDimension(widthSize, height);
         }
     }
@@ -154,58 +190,94 @@ public class PagerTunnelStepsStrip
         if (pagerAdapter == null || pagerAdapter.getCount() == 0)
             return;
 
-        //Paints that will be use to draw our component.
-        final Paint stepColorPaint = new Paint();
-        stepColorPaint.setColor(Color.GRAY); //TODO
-        final Paint enabledStepColorPaint = new Paint();
-        enabledStepColorPaint.setColor(Color.GREEN); //TODO
-
         //Number that will be displayed for the next step that will be drawn
         int nextDrawnStepNumber = 1;
 
         //Then we iterate on pages in the pagerAdapter to know if we should draw
         //a step or a substep. The title of the page will determine this.
         for (int pagePosition = 0; pagePosition < pagerAdapter.getCount(); pagePosition++) {
-            //Color that will be used to draw the dot representing this step/substep
-            final Paint dotColorPaint;
             //Radius of the dot depending on the page being a step or a substep
-            final float dotRadius = stepDotDiameter / 2.0f;
-            //X-coordinates of the center of the dot representing the step/substep
-            final float dotCenterX = (pagePosition + 1) * (getWidth() / (pagerAdapter.getCount() + 1));
-            //Y-coordinates of the center of the dot representing the step/substep
-            final float dotCenterY = getHeight() / 2.0f;
+            final float circleRadius;
+            //X-coordinates of the center of the shape representing the step/substep
+            final float shapeCenterX = (pagePosition + 1) * (getWidth() / (pagerAdapter.getCount() + 1))
+                    + getPaddingTop();
+            //Y-coordinates of the center of the shape representing the step/substep
+            final float shapeCenterY = getHeight() / 2.0f + getPaddingLeft();
 
-            //If we have passed this step, we use the enable color otherwise we use the other color.
-            //When the user is dragging the page, we make a color transition effect.
-            if(pagePosition <= lastPosition) {
-                dotColorPaint = enabledStepColorPaint;
-                stepColorPaint.setAlpha(0);
-            } else if(pagePosition == (lastPosition + 1) && lastPositionOffset != 0.0f) {
-                dotColorPaint = enabledStepColorPaint;
-                dotColorPaint.setAlpha((int) (lastPositionOffset * 255));
-                stepColorPaint.setAlpha((int) (255 - (lastPositionOffset * 255)));
+            //If we have passed this step, we use the foreground color otherwise we use the background color.
+            //When the user is dragging the page, we make a color transition effect using alpha.
+            if (pagePosition <= lastPosition) {
+                stepPaint.setAlpha(255);
+                stepBackgroundPaint.setAlpha(0);
+            } else if (pagePosition == (lastPosition + 1) && lastPositionOffset != 0.0f) {
+                stepPaint.setAlpha((int) (lastPositionOffset * 255));
+                stepBackgroundPaint.setAlpha((int) (255 - (lastPositionOffset * 255)));
             } else {
-                dotColorPaint = stepColorPaint;
-                stepColorPaint.setAlpha(255);
+                stepBackgroundPaint.setAlpha(255);
+                stepPaint.setAlpha(0);
             }
 
             if (STEP_TITLE.equals(pagerAdapter.getPageTitle(pagePosition))) {
-
-                nextDrawnStepNumber ++;
+                circleRadius = stepDiameter / 2.0f;
+                //We draw the shape for this step
+                drawStepShapeOnCanvas(canvas, shapeCenterX, shapeCenterY, circleRadius);
+                //And we draw the step number over
+                //TODO
+                nextDrawnStepNumber++;
             } else if (SUBSTEP_TITLE.equals(pagerAdapter.getPageTitle(pagePosition))) {
-
+                circleRadius = subStepDiameter / 2.0f;
+                //We draw the shape for this substep
+                drawStepShapeOnCanvas(canvas, shapeCenterX, shapeCenterY, circleRadius);
             } else
                 throw new IllegalStateException(pagerAdapter.getPageTitle(pagePosition)
                         + " is not recognized as a step or substep title. Please use STEP_TITLE and SUBSTEP_TITLE as title for your page.");
-            //Finally, we draw our step/substep on our canvas
-            final RectF dotRect = new RectF(dotCenterX - dotRadius, dotCenterY - dotRadius,
-                    dotCenterX + dotRadius, dotCenterY + dotRadius);
-            //We draw the background dot only if its alpha is greater than 0
-            if(stepColorPaint.getAlpha() > 0)
-                canvas.drawOval(dotRect, stepColorPaint);
-            canvas.drawOval(dotRect, dotColorPaint);
-
         }
+    }
+
+    /**
+     * Draw the step/substep shape on the canvas centered on the provided position.
+     *
+     * @param canvas       Canvas where the shape will be drawn
+     * @param shapeCenterX X-Coordinates of the center of the shape
+     * @param shapeCenterY Y-Coordinates of the center of the shape
+     * @param circleRadius Size of the shape in px.
+     */
+    private void drawStepShapeOnCanvas(Canvas canvas, float shapeCenterX, float shapeCenterY, float circleRadius) {
+        //Finally, we draw our step/substep on our canvas
+        final RectF stepShapeRect = new RectF(shapeCenterX - circleRadius, shapeCenterY - circleRadius,
+                shapeCenterX + circleRadius, shapeCenterY + circleRadius);
+        //We draw the background dot only if its alpha is greater than 0
+        if (stepBackgroundPaint.getAlpha() > 0)
+            canvas.drawOval(stepShapeRect, stepBackgroundPaint);
+        if(stepPaint.getAlpha() > 0)
+            canvas.drawOval(stepShapeRect, stepPaint);
+    }
+
+    public int getStepColor() {
+        return stepColor;
+    }
+
+    public void setStepColor(int stepColor) {
+        this.stepColor = stepColor;
+        this.stepPaint.setColor(stepColor);
+    }
+
+    public int getStepBackgroundColor() {
+        return stepBackgroundColor;
+    }
+
+    public void setStepBackgroundColor(int stepBackgroundColor) {
+        this.stepBackgroundColor = stepBackgroundColor;
+        this.stepBackgroundPaint.setColor(stepBackgroundColor);
+    }
+
+    public int getTextColor() {
+        return textColor;
+    }
+
+    public void setTextColor(int textColor) {
+        this.textColor = textColor;
+        this.textPaint.setColor(textColor);
     }
 
     /**
---
+++ service/src/main/java/fr/cvlaminck/notidroid/android/base/fragments/account/signin/SignInCredentialsFragment.java
@@ -0,0 +1,14 @@
+package fr.cvlaminck.notidroid.android.base.fragments.account.signin;
+
+import android.app.Fragment;
+
+import org.androidannotations.annotations.EFragment;
+
+/**
+ * Fragment where the user can provide its email address and its password that
+ * will be used as its credentials.
+ */
+@EFragment
+public class SignInCredentialsFragment
+        extends Fragment {
+}
